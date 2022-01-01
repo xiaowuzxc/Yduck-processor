9602f546bf4da8aa03a5ece181426849a1205e13
xiaowuzxc <wuyan-100@163.com>
2022-01-01 13:19:29 +0800

    添加指令汇编器

--- tools/asm/Assembler.py
+++ tools/asm/Assembler.py
@@ -1,6 +1,14 @@
 #python边学边做
 #最终将会做一个汇编器
 import os,re
+#定义指令字典
+YDcmd={'NF':'0000','LD':'0001','SV':'0010','IN':'0011','SW':'0100','WR':'0101','CR':'0110',\
+'LA':'0111','LO':'1000','AD':'1001','SB':'1010','JW':'1011','JA':'1100','LL':'1101','LR':'1110','TL':'1111'}
+#定义寄存器字典
+YDreg={'ZE':'0000','DK':'0001','R0':'0010','R1':'0011','R2':'0100','R3':'0101','R4':'0110','R5':'0111',\
+'R6':'1000','R7':'1001','R8':'1010','R9':'1011','RA':'1100','RB':'1101','RC':'1110','PC':'1111'}
+#定义伪指令
+YDwcm={'NOP':'00000000'}
 
 def 检查out文件():
 	try:
@@ -33,7 +41,6 @@ def 指令预处理():
 				inistr=inistr[:cntstr]#储存结束符前的数据
 				break#结束遍历
 			cntstr+=1#无事发生，下一个字符
-		print(inistr)
 		if cntstr:#如果此行有有效内容
 			data[行指针]=inistr#存回data
 		else:#此行是空白
@@ -60,15 +67,16 @@ def 语法检查():
 			print("错误:out.txt 第",行指针+1,"行存在非法字符")
 		else:
 			pass
-		ckstr=re.match(r'NF|LD|SV|IN|SW|NOP',inistr)
+		ckstr=re.match(r'NF|LD|SV|IN|SW|NOP',inistr)#检测开头是否匹配8b指令
 		if ckstr:#8b/NOP指令
-			ckstr=re.match(r'((((NF|LD|SV|IN|SW),(ZE|DK|PC|R[0-9|A|B|C]))|NOP);(((NF|LD|SV|IN|SW),(ZE|DK|PC|R[0-9|A|B|C]))|NOP))|NOP',inistr)
+			ckstr=re.match(r'((((NF|LD|SV|IN|SW),(ZE|DK|PC|R[0-9|A|B|C]))|NOP);(((NF|LD|SV|IN|SW),(ZE|DK|PC|R[0-9|A|B|C]))|NOP))|NOP'\
+			,inistr)
 			if ckstr:#匹配8b语法
 				pass
 			else:#不匹配
 				print("错误:out.txt 第",行指针+1,"行指令语法错误")
 		else:#16b指令
-			ckstr=re.match(r'WR|CR|LA|LO',inistr)
+			ckstr=re.match(r'WR|CR|LA|LO',inistr)#检测是否匹配16b CMD,RG1,RG2语法
 			if ckstr:#16b指令CMD,RG1,RG2
 				ckstr=re.match(r'(WR|CR|LA|LO),(ZE|DK|PC|R[0-9|A|B|C]),(ZE|DK|PC|R[0-9|A|B|C])',inistr)
 				if ckstr:
@@ -87,32 +95,40 @@ def 语法检查():
 					print("错误:out.txt 第",行指针+1,"行立即数位宽错误")
 		行指针+=1#切换下一行
 
+
+
+def 立即数转换(Tdec):
+	pass
+
 def 执行汇编():
 	obj=open('./obj.txt','w+')
 	for strdata in data:
-		strdata=strdata+"0v0"
-		obj.write(strdata)#写入
+		objdata=""#输入字符串清零
+		ckstr=re.search(r'NF|LD|SV|IN|SW|NOP',strdata)#检测是否匹配8b/NOP指令
+		if ckstr:#8b/NOP指令
+			if re.match(r'NOP',strdata) and len(strdata) < 4:#只有NOP
+				objdata=YDwcm['NOP']+YDwcm['NOP']#一个周期空指令
+			else:#CMD,RG;XX
+				objdata=YDcmd[strdata[0:2]]+YDreg[strdata[3:5]]#第一个8b指令输入
+				if re.match(r'NOP',strdata[6:]):
+					objdata=objdata+YDwcm['NOP']
+				else:
+					objdata=objdata+YDcmd[strdata[6:8]]+YDreg[strdata[9:]]
+		else:#16b指令
+			if re.match(r'WR|CR|LA|LO',strdata):#16b指令CMD,RG1,RG2
+				objdata=YDcmd[strdata[0:2]]+YDreg[strdata[3:5]]+YDreg[strdata[6:8]]+'0000'
+			else:#16b指令CMD,RG,$H
+				objdata=YDcmd[strdata[0:2]]+YDreg[strdata[3:5]]+"{:b}".format(int(strdata[6:])).zfill(8)
+		obj.write(objdata)#写入
 		obj.write('\n')#换行
 	obj.close()
-'''
-		inistr=data[行指针]#data的对应行存入inistr
-		cntstr=0#字符指针
-		while cntstr<len(inistr):#遍历整行，将助记符转换为二进制
-			if inistr[cntstr]=='#' or inistr[cntstr]=='\n' or inistr[cntstr]=='\r':#如果有#或换行
-				inistr=inistr[:cntstr]#储存结束符前的数据
-				break#结束遍历
-			cntstr+=1#无事发生，下一个字符
-'''
 
 
 检查out文件()#如果已存在out文件，则重新生成
-with open('./asm.txt','r+') as f:#打开test.txt文件
+with open('./asm.txt','r+',encoding='UTF-8') as f:#打开test.txt文件
 	data=f.readlines()#按行读取，生成list至data
 f.closed#关闭文件
 指令预处理()#预处理data，去除注释、多余行、空格
 语法检查()
 执行汇编()
-print('over')
-'''
-
-'''
+print('结束汇编')
--- tools/asm/test/run.bat
+++
@@ -1,4 +0,0 @@
-chcp 65001
-echo "执行汇编，转换asm到二进制"
-python Assembler.py
-pause
