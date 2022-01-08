4d8552d45e79425ed3337c19d73335e2be29afbc
xiaowuzxc <wuyan-100@163.com>
2022-01-08 21:12:47 +0800

    通道0，1级译码完成

--- RTL/YD_core.v
+++ RTL/YD_core.v
@@ -17,7 +17,8 @@ module YD_core
 //寄存器定义
 reg jpc;//跳转指示
 reg dwi;//数据空间写指示
-reg dsw;//双发射指示
+reg dsw;//双发射1级指示
+reg dsw_r;//双发射2级指示
 reg [DW-1:0]idata;//第一阶段指令
 reg [DW-1:0]idata_r;//指令寄存
 reg [15:0]din0;
@@ -77,7 +78,6 @@ always @(posedge clk) begin
 end
 /*
 output 	[AW-1:0]	d_addr, //地址输入
-output				d_we, //高电平写使能
 
 //输出口0
 	input wire [3:0]raddr0,
@@ -88,20 +88,22 @@ output				d_we, //高电平写使能
 */
 //指令译码，生成读地址
 always @(*) begin
-	d_addr=16'h0;
-	d_we=1'b0;
 	raddr0=4'h0;
 	raddr1=4'h0;
-	case (idata[DW-1:DW-4])
+	if(dwi) d_addr=16'hz;
+	case (idata[15:12])
 		NF:begin
 			dsw=1'b1;//当前为8b指令
 			raddr0=idata[11:8];
 			end
 		LD:begin
 			dsw=1'b1;
-			d_we=1'b1;
 			raddr0=DKA;
-			d_addr=dout0;
+			if(dwi) 
+				d_addr=16'hz;//数据空间正在写
+
+			else
+				d_addr=dout0;//数据空间没有动作
 			end
 		SV:begin
 			dsw=1'b1;
@@ -183,9 +185,106 @@ input [DW-1:0]	din, //数据输入
 
 	output reg [15:0]dout0,//寄存器读取，数据输出线，always@(*)只能给reg赋值
 */
-//通道1，取得操作数值，生成写地址
+//取得操作数值，生成写地址
+always @(*) begin
+	d_din=16'h0;
+	if(~dwi) d_addr=16'h0;
+	d_we=1'b0;
+	din0=16'h0;
+	waddr0=4'h0;
+	we0=1'b0;
+	din1=16'h0;
+	waddr1=4'h0;
+	we1=1'b0;
+	case (idata_r[15:12])
+		NF:begin
+			dsw_r=1'b1;//当前为8b指令
+			raddr0=idata[11:8];
+			end
+		LD:begin
+			dsw_r=1'b1;
+			raddr0=DKA;
+			if(dwi) 
+				d_addr=16'hz;//数据空间正在写
 
+			else
+				d_addr=dout0;//数据空间没有动作
+			end
+		SV:begin
+			dsw_r=1'b1;
+			raddr0=idata[11:8];
+			end
+		IN:begin
+			dsw_r=1'b1;
+			raddr0=idata[11:8];
+			end
+		SW:begin
+			dsw_r=1'b1;
+			raddr0=idata[11:8];
+			end
+		WR:begin
+			dsw_r=1'b0;//当前为16b指令
+			raddr0=idata[11:8];
+			end
+		CR:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			raddr1=idata[7:4];
+			end
+		LA:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			raddr1=idata[7:4];
+			end
+		LO:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			raddr1=idata[7:4];
+			end
+		AD:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		SB:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		JW:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		JA:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		LL:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		LR:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+		TL:begin
+			dsw_r=1'b0;
+			raddr0=idata[11:8];
+			end
+	endcase
+end
 
+//内部数据寄存
+always @(posedge clk) begin
+	if(rst) begin
+		dwi <= 1'b0;
+		jpc <= 1'b0;
+		end 
+	else begin
+		if(idata[15:12]==JA || idata[15:12]==JW)
+			jpc <= 1'b1;
+		if(idata[15:12]==SV)
+			dwi <= 1'b1;
+		end
+end
 
 assign i_addr=PC;//地址指示
 YD_reg u_YD_reg
