a3c0ee11629c95bfcf3ed03a1356e0e71ff108c5
xiaowuzxc <wuyan-100@163.com>
2022-02-08 12:38:27 +0800

    autoset

--- tools/setenv/autoset.sh
+++ tools/setenv/autoset.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt install make git python3 gtkwave gcc g++ bison flex gperf autoconf
+sudo apt install make git python3 gtkwave gcc g++ bison flex gperf autoconf
 git clone -b v11_0 --depth=1 https://gitee.com/xiaowuzxc/iverilog/
 cd iverilog
 sh autoconf.sh
